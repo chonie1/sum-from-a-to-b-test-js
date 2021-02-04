e7e0ec4258d7224a8ce8df09bcf912d55c4ba761
Connie Ho <connie3@ualberta.ca>
2021-02-04 17:16:58 +0000

    updated sum

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN > toN) {
+    return 0;
+  } else if (fromN === toN) {
+    return fromN;
+  }
+
+  return fromN + toN + sum(fromN + 1, toN - 1);
 }
+
+console.log(sum(0,1));
+console.log(sum(1,2));
+console.log(sum(3,7));
 
 module.exports = sum;
